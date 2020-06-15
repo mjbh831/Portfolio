baac245ce7a56d816359ff7230be8fb6e2c70416
mjbh831 <56654912+mjbh831@users.noreply.github.com>
2020-06-15 10:57:56 -0500

    edited bio page

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -17,7 +17,7 @@ const AboutMe = () => {
   let aboutMeObj = {
     title: "About Me",
     info:
-      "I'm a self-taught Web Developer born and raised in Dallas. After graduating from culinary school and spending a few years in the restaurant industry, I found my true passion for coding. Using Udemy, YouTube, and Harvard's CS50 online course, I've spent over 1000 hours studying computer science and web development. I now create user-friendly applications while striving to write readable and scalable code. I love to have fun and don't take myself too seriously. My years of being in the service industry have made me a strong communicator, hard-worker, and team player. I am also a huge believer in life-long learning and look forward to continuing to grow as a developer.",
+      "Hello and thanks for visiting my page. I am a self-taught web developer born and raised in Dallas, Texas. After getting my CompTIA A+ certification a few years back and working in a tech support role, I found my true passion for coding. I began using Udemy, YouTube, and Harvard's CS50 online courses to learn to be a programmer. I've spent over 1000 hours studying computer science and web development, and now I create user-friendly applications while striving to write readable and scalable code. My years of being in the service industry have made me a strong communicator, hard-worker, and team player. I am also a huge believer in life-long learning and look forward to continuing to grow as a developer.",
     backgroundText: "About Me",
     backgroundColor: "#5cf4ff",
     projectPic: fixedState,
