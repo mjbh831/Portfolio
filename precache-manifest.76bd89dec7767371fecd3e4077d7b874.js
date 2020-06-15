09dd87eeb4fdf3bedc01631dbf83a7cae1731b9f
mjbh831 <56654912+mjbh831@users.noreply.github.com>
2020-06-15 09:24:51 -0500

    Updates

---
+++ precache-manifest.76bd89dec7767371fecd3e4077d7b874.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5f64b4d04049412e68ea25145afb4b",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "4f313be2d0fdba5a7e5a",
+    "url": "/Portfolio/static/css/main.0ce4c4fb.chunk.css"
+  },
+  {
+    "revision": "b628829120e82ddab16a",
+    "url": "/Portfolio/static/js/2.bbaf672f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.bbaf672f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f313be2d0fdba5a7e5a",
+    "url": "/Portfolio/static/js/main.656149df.chunk.js"
+  },
+  {
+    "revision": "bf904ee637eb89ecd0df",
+    "url": "/Portfolio/static/js/runtime-main.fe6fd8b2.js"
+  },
+  {
+    "revision": "ffb9467a3cdb77a7d5bfaca3af6d12af",
+    "url": "/Portfolio/static/media/AboutMe.ffb9467a.png"
+  },
+  {
+    "revision": "07ad5b5a55ce2eb3d03252b624a921e9",
+    "url": "/Portfolio/static/media/AboutMeTablet.07ad5b5a.png"
+  },
+  {
+    "revision": "541e976e75f3d13004cd9b7f48cce3cd",
+    "url": "/Portfolio/static/media/FaceApp.541e976e.png"
+  },
+  {
+    "revision": "0a3b10e86c6edb84fcd814536bc9a6f4",
+    "url": "/Portfolio/static/media/FaceAppTablet.0a3b10e8.png"
+  },
+  {
+    "revision": "b247fdcadf37f5ff0f27ce8322e41912",
+    "url": "/Portfolio/static/media/MarcosHinojosResume.b247fdca.pdf"
+  },
+  {
+    "revision": "ebffa5b4ffc7e59c05bc8dd6756769af",
+    "url": "/Portfolio/static/media/RN-project.ebffa5b4.png"
+  },
+  {
+    "revision": "ff42e092a9256e6f451b5a17f3f34e7e",
+    "url": "/Portfolio/static/media/RN-tablet.ff42e092.png"
+  },
+  {
+    "revision": "fcb35d368756f139c3333468a7fdb44a",
+    "url": "/Portfolio/static/media/RoboFriends.fcb35d36.png"
+  },
+  {
+    "revision": "bc37a52eb8efa03e5077c8c4dc94e713",
+    "url": "/Portfolio/static/media/RoboFriendsTablet.bc37a52e.png"
+  },
+  {
+    "revision": "dcfd033e91ddd013f50abf78e0dff970",
+    "url": "/Portfolio/static/media/SF-project.dcfd033e.png"
+  },
+  {
+    "revision": "b4e9232e643047dc74a31551460aaf91",
+    "url": "/Portfolio/static/media/SF-projectTablet.b4e9232e.png"
+  },
+  {
+    "revision": "23c55694a7a84c4031f7d180fde0cb21",
+    "url": "/Portfolio/static/media/fourdoortour.23c55694.png"
+  },
+  {
+    "revision": "2b7dd67b9a9cbb98b7cb910d7eb805f1",
+    "url": "/Portfolio/static/media/fourdoortourTablet.2b7dd67b.png"
+  },
+  {
+    "revision": "e77952f79a66b0bdaa89663d74f7689c",
+    "url": "/Portfolio/static/media/postIt.e77952f7.png"
+  },
+  {
+    "revision": "05d5c0ec76ee65279f82d7fbb1742168",
+    "url": "/Portfolio/static/media/postitTablet.05d5c0ec.png"
+  }
+]);
