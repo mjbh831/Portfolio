43f48ddf24d7ee60be57c5f850a150d446a34af0
mjbh831 <56654912+mjbh831@users.noreply.github.com>
2020-06-15 09:49:05 -0500

    updated resume with correct portfolio

--- src/Components/Buttons/Buttons.jsx
+++ src/Components/Buttons/Buttons.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Resume from "../../../src/MarcosHinojosResume.pdf";
+import Resume from "../../../src/MJBH-Resume.pdf";
 // Enter 'title' or 'contact' for which button
 
 export const linkedInTitle = () => (
